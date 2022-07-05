70b74ce4dfd7ce4a2f45ecdf13176eda9e89f10c
pythearn <ermandmani@gmail.com>
2022-07-05 23:36:17 +0100

    a

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,13 +55,21 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-print
+print(users['Jonathan']['twitter'])
 # 2. Get Erik's hometown
+print(users['Erik']['hometown'])
 # 3. Get the list of Erik's lottery numbers
+print(users['Erik']['lottery_numbers'])
 # 4. Get the species of Avril's pet Monty
+print(users['Avril']['pets']['species'])
 # 5. Get the smallest of Erik's lottery numbers
+
 # 6. Return an list of Avril's lottery numbers that are even
+
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
